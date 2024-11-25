9c0496eed2b0ffaa463ca05b7643cc69810b6048
Joao-lucas-felix <jlmacena06@gmail.com>
2024-11-25 19:48:58 -0300

    feat: Create the get Methods

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crud-basico/server"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -13,6 +14,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
-
+	router.HandleFunc("/users", server.FindAllUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.FindAllUsers).Methods(http.MethodGet)
+	fmt.Println("The applicationa are running in the port: 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
