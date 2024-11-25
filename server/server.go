41f15dce444987241468df247163c856bbfbb44e
Joao-lucas-felix <jlmacena06@gmail.com>
2024-11-25 19:49:27 -0300

    feat: Create the get Methods

--- server/server.go
+++ server/server.go
@@ -60,7 +60,46 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf("User are created successfully Number of rows afected: %d", rowsAffected)))
+	w.Write([]byte(fmt.Sprintf("User are created successfully Number of rows afected: %d", rowsAffected)))	
+}
+// Find all users in the data base
+func FindAllUsers(w http.ResponseWriter, r * http.Request){
+	db, err := database.Connect()
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error while trying to connect with DataBase"))
+		return
+	}
+	defer db.Close()
 
+	rows, err := db.Query("SELECT * FROM usuarios u")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error while trying to get the users in DataBase!"))
+		return
+	}
+	defer rows.Close()
+
+	var users []user
+
+	for rows.Next()	 {
+		var user user
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil{
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error while trying to convert the user of DataBase response!"))
+			return
+		}
+		users = append(users, user)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error while trying to convert the user to json"))
+		return
+	} 
 
 }
+//Search one user in the data base
+func FindUser(w http.ResponseWriter, r * http.Request){}
