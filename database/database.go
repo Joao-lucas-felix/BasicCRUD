600332070c8a2f8a2d3713e67fa1d3612fc983b8
Joao-lucas-felix <jlmacena06@gmail.com>
2024-12-02 19:43:34 -0300

    Refactor: Comment the function

--- database/database.go
+++ database/database.go
@@ -5,7 +5,7 @@ import (
 
 	_ "github.com/lib/pq" //Go driver connect
 )
-
+//this function try to connect with the Data Base and return the connection
 func Connect() (*sql.DB, error) {
 	strConnection := "user=golang dbname=devbook password=golang host=localhost port=5432 sslmode=disable"
 	db, err := sql.Open("postgres", strConnection)
